a5fe7f9645b045fd5094028b481c9ea2670ce239
doctork4 <doctor.k1994@gmail.com>
2023-05-30 14:48:36 +0900

    feat : Header 컴포넌트 버튼 태그 라우팅 기능 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,30 @@
-import { JsxElement } from 'typescript'
+import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.scss'
 
 export const Header:React.FC = () => {
+  const router = useRouter();
+
+  const handleToShop = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    router.push("/shop");
+  };
+  
+  const handleToCart = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    router.push("/cart");
+  };
+
   return (
     <header className={styles.headerwrap}>
       <div className={styles.wrapper}>
         <div className={styles.buttonwrap}>
-          <button className={styles.shopbutton} type="button"></button>
+          <button className={styles.shopbutton} type="button" onClick={handleToShop}></button>
         </div>
         <div className={styles.title}>
           <h1>Refilled</h1>
         </div>
         <div className={styles.buttonwrap}>
-          <button className={styles.cartbutton} type="button"></button>
+          <button className={styles.cartbutton} type="button" onClick={handleToCart}></button>
         </div>
       </div>
     </header>
